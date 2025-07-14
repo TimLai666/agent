2ad7d7d25db8e2fb0415093dba83db5d20825ce4
曾偉杰465828 <fineline.jason@gmail.com>
2025-07-14 23:53:34 +0800

    新增語音輸入feature

--- main.py
+++ main.py
@@ -5,18 +5,33 @@ from pydantic_ai.models.openai import OpenAIModel
 from pydantic import BaseModel
 from dotenv import load_dotenv
 import os
-
+import speech_recognition as sr
 from internal.tools.tools import add_all_tools
+
 
 
 class CityLocation(BaseModel):
     city: str
     country: str
 
+def Voice_To_Text(duration=5):  
+    r = sr.Recognizer() 
+    with sr.Microphone() as source: 
+        print("請開始說話:") 
+        r.adjust_for_ambient_noise(source) 
+        audio = r.listen(source, phrase_time_limit=duration) 
+    try: 
+        Text = r.recognize_google(audio, language="zh-TW") 
+    except sr.UnknownValueError: 
+        Text = "無法翻譯" 
+    except sr.RequestError as e: 
+        Text = "無法翻譯{0}".format(e) 
+    return Text 
+
 
 def main() -> None:
     load_dotenv()
-
+    print(Voice_To_Text())
     ollama_model = OpenAIModel(
         model_name='qwen3:14b', provider=OpenAIProvider(base_url=f'{os.getenv("OLLAMA_BASE_URL")}/v1')
     )
